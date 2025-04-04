5259badab469d7081ef9e9fb4c9136a707517588
matthewromaniuk <matthewromaniukk@gmail.com>
2025-04-04 15:41:01 -0500

    updated cat_bot to use array of links rather than reading every 24 hours

--- TODO.md
+++
@@ -1,2 +0,0 @@
-## TODO
-Use database to fix url storage issues, and allow for urls to be added
--- cat_bot.py
+++ cat_bot.py
@@ -15,7 +15,12 @@ intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-f = open("urls.txt", "r")
+urls = []
+index = 0
+with open("urls.txt", "r") as f:
+    urls = f.read().split("\n")
+    
+
 
 @bot.event
 async def on_ready():
@@ -24,12 +29,20 @@ async def on_ready():
 @bot.command()
 async def unleash(ctx):
     print('Starting...')
-    sendpic.start(ctx)
+    send_img.start(ctx)
 
 @bot.command()
 async def nextimg(ctx):
-    url = f.readline().strip()
+    await send_img(ctx)
+
+@tasks.loop(seconds=5)
+async def send_img(ctx):
+    global index
+    global urls
+    url = urls[index]
+    index += 1
     response = requests.get(url, headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36' })
+
     if response.status_code == 200:
         img = Image.open(BytesIO(response.content))
         buffer = BytesIO()
@@ -38,20 +51,7 @@ async def nextimg(ctx):
         buffer.seek(0)
         file = discord.File(buffer, filename="image.png")
         await ctx.send(file=file)
-
-@tasks.loop(hours=24)
-async def sendpic(ctx):
-    url = f.readline().strip()
-    response = requests.get(url, headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36' })
-    if response.status_code == 200:
-        img = Image.open(BytesIO(response.content))
-        buffer = BytesIO()
         
-        img.save(buffer, format="PNG")
-        buffer.seek(0)
-        file = discord.File(buffer, filename="image.png")
-        await ctx.send(file=file)
-
     
 
 bot.run(TOKEN)
